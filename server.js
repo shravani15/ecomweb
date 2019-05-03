25c6c78ba068d06d9fc423c01c6b74bb9e1e60c9
shravani reddy <shravani0716@gmail.com>
2019-05-03 09:45:16 +0530

    updating changes

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const path = require('path');
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({extended:true}))
+
 app.use('/',express.static(path.join(__dirname,'public')))
 app.use('/api', require('./routes/api').route)
 
