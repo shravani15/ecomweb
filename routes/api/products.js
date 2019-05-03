f21678a3003759b1b4b01b66926de8b7e4fa4906
shravani reddy <shravani0716@gmail.com>
2019-05-03 18:23:26 +0530

    updating products and users part

--- routes/api/products.js
+++ routes/api/products.js
@@ -1,4 +1,37 @@
 const Product = require('../../db').Product
 const route = require('express').Router();
 
-exports = module.exports = route
+route.get('/',(req,res) => {
+    Product.findAll()
+        .then((products) => {
+            res.status(200).send(products)
+        })
+        .catch((err) =>{
+            res.status(500).send({
+                error: "could not retrieve products"
+            })
+        }) 
+})
+
+route.post('/',(req,res) => {
+    if(isNaN(req.body.price)) {
+        return res.status(403).send({
+            error: "price is not a valid number"
+        })
+    }
+
+    Product.create({
+        name : req.body.name,
+        manufacturer: req.body.manufacturer,
+        price :parseFloat(req.body.price)
+    }).then((product) => {
+        res.status(201).send(product)
+    })
+    .catch((err) => {
+        res.status(501).send({
+            error: "could not add a product"
+        })
+    })
+})
+
+module.exports = route; 
